47e02482e4c4601afdde459376e7883dd2c14c69
Milton Muñoz <livemilton@gmail.com>
2024-08-30 22:48:46 -0500

    add yml for all microservices an configure the classpath

--- src/main/java/com/livemilton/configserver/ConfigserverApplication.java
+++ src/main/java/com/livemilton/configserver/ConfigserverApplication.java
@@ -2,10 +2,10 @@ package com.livemilton.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
-@EnabledConfigServer
-
+@EnableConfigServer
 public class ConfigserverApplication {
 
 	public static void main(String[] args) {
